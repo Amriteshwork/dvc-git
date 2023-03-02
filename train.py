4ea991c16de5c8d83e06af14fe07f239b91fb835
Amritesh <amriteshwork@gmail.com>
2023-03-03 00:28:35 +0530

    second commit

--- train.py
+++ train.py
@@ -1,4 +1,8 @@
 def say_hello(name):
     print("Hello {}, the Data Scientist".format(name))
 
+def greeting(name):
+    print("Hello {}, the Data Scientist".format(name))
+
 say_hello("Amritesh")
+greeting("Amritesh")
